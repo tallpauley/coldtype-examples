1a6ef8ff22cad282d270c12db507a6bf5f850bd8
Rob Stenson <rob.stenson@gmail.com>
2020-04-16 13:02:37 -0700

    banner tweak

--- animations/banner.py
+++ animations/banner.py
@@ -4,7 +4,7 @@ states = [
     dict(wdth=0, rotate=0, tu=300),
     dict(wdth=1, rotate=15, tu=-150),
     dict(wdth=0.25, rotate=-15, tu=50),
-    dict(wdth=0.75, rotate=0, tu=-475),
+    dict(wdth=0.75, rotate=-25, tu=-375),
     dict(wdth=0.5, rotate=25, tu=100)
 ]
 
@@ -15,4 +15,4 @@ loop = Loop(200, len(states), 10)
 def render(f):
     phase = f.a.t.current_phase(f.i)
     state = phase.calc_state(states)
-    return StyledString("COLDTYPE", Style(obvs, 250, fill=0, **state, r=1)).pens().align(f.a.r).f(0).understroke(s=1, sw=3)
+    return StyledString("COLDTYPE", Style(obvs, 250, fill=0, **state, r=1)).pens().align(f.a.r).f(0).understroke(s=1, sw=15)
