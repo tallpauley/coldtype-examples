65c40e18e94b53c302601bb5cd3f08ce40b185ef
Rob Stenson <rob.stenson@gmail.com>
2020-05-15 10:18:39 -0700

    note on why audio isn't great

--- animations/audio.py
+++ animations/audio.py
@@ -5,6 +5,31 @@ from coldtype import *
 
 """
 You'll need to `pip install soundfile` to get this to work
+
+N.B. Though animating to audio _seems_ like a good idea, and can
+be executed nicely, given the difference between how the human
+brain processes sound and how the human brain processes images,
+I’ve found it’s usually better to power an "audio" animation using
+non-audio sources, like a MIDI file, or just frame-numbers, both
+because actual audio data is rarely as clean as you’d like it to be,
+but also because audio data is simply more difficult to understand
+and work with. Using sparse non-audio data, you can ease-in
+and ease-out of "hits" — this is important, because audio data
+itself almost never "eases in," even though we crave "ease-in"
+information when watching music; think of a drummer hitting a
+drum, or a guitarist strumming — we’re seeing them ease-in to
+the sounds, but there is no sound before the sound. (Also our
+brains process sound wayyy faster than our eyeballs can process
+site, which explains why we only need 24 images a second to be
+fooled, but thousand and thousands of samples.)
+
+This is why there’s not canonical class for reading audio data in
+the coldtype library itself.
+
+But!
+
+Sometimes it is fun to read an actual audio file.
+So here’s an example of that.
 """
 
 class Wavfile():
